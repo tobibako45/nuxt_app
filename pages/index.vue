ea0b3f31cf9758a92be81a8c79ad4c553110a5ba
tobibako45 <tobibako45@gmail.com>
2019-08-08 18:59:35 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,49 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt_app
-      </h1>
-      <h2 class="subtitle">
-        My extraordinary Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <hr />
+    <pre>[{{now}}]</pre>
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data: function() {
+    return {
+      title: "Hello",
+      message: "this is message.",
+      now: "wait..."
+    };
+  },
+  created: function() {
+    // JSのタイマー
+    setInterval(() => {
+      var d = new Date();
+      this.now = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    }, 1000);
   }
-}
+};
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 5px 10px;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+h1 {
+  font-size: 60pt;
+  color: #345980;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+p {
+  padding-top: 5px;
+  font-size: 20pt;
 }
-
-.links {
-  padding-top: 15px;
+pre {
+  padding: 10px;
+  font-size: 18pt;
+  background-color: #efefef;
+}
+hr {
+  margin: 10px 0px;
 }
 </style>
