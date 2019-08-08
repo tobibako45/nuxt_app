c1fb12a52f09a0dd4a47de072810e71787b058b1
tobibako45 <tobibako45@gmail.com>
2019-08-08 19:31:22 +0900

    Add pass.vue

---
+++ pages/p/_id/_pass.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <p v-html="message">no message!</p>
+    <hr />
+
+    <div class="right">
+      <router-link to="/">TOP</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: "Login"
+    };
+  },
+  computed: {
+    message: function() {
+      let id =
+        this.$route.params.id != undefined
+          ? this.$route.params.id
+          : "NO ID だよ";
+      let pass =
+        this.$route.params.pass != undefined
+          ? this.$route.params.pass
+          : "No Pass だよ";
+      return "ID: " + id + "<br>PASS:" + pass;
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  padding: 5px 10px;
+}
+h1 {
+  font-size: 60pt;
+  color: #345980;
+}
+p {
+  padding-top: 5px;
+  font-size: 20pt;
+}
+a {
+  font-size: 16pt;
+}
+.right {
+  text-align: right;
+}
+</style>
