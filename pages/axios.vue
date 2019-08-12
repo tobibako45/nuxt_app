b1d70108d247c601c952bd065cbfb19f071318c1
tobibako45 <tobibako45@gmail.com>
2019-08-12 17:52:57 +0900

    6-3

--- pages/axios.vue
+++ pages/axios.vue
@@ -1,11 +1,17 @@
 <template>
-  <sction class="container">
+  <section class="container">
     <h1>{{title}}</h1>
     <p>{{message}}</p>
-
+    <div>
+<!--      v-model="msgで、this.msgに保管-->
+      <input type="text" v-model="msg" />
+<!-- @clickでdoClickを呼び出す-->
+      <button @click="doClick">Click</button>
+    </div>
+    <hr>
     <table>
       <tr>
-        <th>UserID</th>
+        <th>User ID</th>
         <td>{{json_data.userId}}</td>
       </tr>
       <tr>
@@ -21,10 +27,7 @@
         <td>{{json_data.body}}</td>
       </tr>
     </table>
-
-
-  </sction>
-
+  </section>
 </template>
 
 <script>
@@ -37,14 +40,27 @@
             return {
                 title: 'Axios',
                 message: 'axios sample',
+                msg: '',
+                json_data: {}
             }
         },
-        async asyncData() {
-            let id = 10; // id番号
-            // await axios.getで、サイトの"https://jsonplaceholder.typicode.com/posts/10"とかにアクセスしてresultに代入。
-            let result = await axios.get(url + id);
-            // result.dataに、オブジェクトとしてマージされている。json_dataに入れてそのまま使える。
-            return {json_data: result.data};
+        // async asyncData() {
+        //     let id = 10; // id番号
+        //     // await axios.getで、サイトの"https://jsonplaceholder.typicode.com/posts/10"とかにアクセスしてresultに代入。
+        //     let result = await axios.get(url + id);
+        //     // result.dataに、オブジェクトとしてマージされている。json_dataに入れてそのまま使える。
+        //     return {json_data: result.data};
+        // }
+
+        methods: {
+            doClick: function (event) {
+                // v-modelで保管されたmsgをthis.msgで取り出して、変数urlにくっつけてアクセスしている。
+                axios.get(url + this.msg).then((res) => {
+                    this.message = 'get ID=' + this.msg;
+                    // then内のアロー関数で、引数のresからdataを取り出し、this.json_dataに設定している
+                    this.json_data = res.data;
+                })
+            }
         }
     }
 </script>
@@ -82,6 +98,12 @@
   tr td {
     padding:5px 10px;
     background-color:#eef;
+    font-size:14pt;
+  }
+  input {
+    font-size:14pt;
+  }
+  button {
     font-size:14pt;
   }
 </style>
