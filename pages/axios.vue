a1f4c50f78eb91cf243d0430c7d79da24beaf223
tobibako45 <tobibako45@gmail.com>
2019-08-12 17:02:06 +0900

    6-1

---
+++ pages/axios.vue
@@ -0,0 +1,54 @@
+<template>
+  <sction class="container">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <pre>{{html_data}}</pre>
+  </sction>
+
+</template>
+
+<script>
+    const axios = require('axios');
+    let url = "/README.md";
+
+    export default {
+        data: function () {
+            return {
+                title: 'Axios',
+                message: 'axios sample',
+            }
+        },
+        asyncData: async function() {
+            let result = await axios.get(url);
+            return {
+                html_data: result.data
+            }
+        }
+    }
+</script>
+
+<style>
+  .container {
+    padding: 5px 10px;
+  }
+  h1 {
+    font-size: 60pt;
+    color: #345980;
+  }
+  p {
+    padding-top:5px;
+    font-size: 20pt;
+  }
+  div {
+    font-size:14pt;
+  }
+  pre {
+    padding: 10px;
+    font-size: 18pt;
+    background-color: #efefef;
+    white-space: pre-wrap ;
+  }
+  hr {
+    margin:10px 0px;
+  }
+</style>
