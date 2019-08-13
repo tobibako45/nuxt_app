6c5d9e379451d08c608a4149c3b928d97e3210bd
tobibako45 <tobibako45@gmail.com>
2019-08-13 18:20:51 +0900

    6-4

--- pages/axios.vue
+++ pages/axios.vue
@@ -3,12 +3,12 @@
     <h1>{{title}}</h1>
     <p>{{message}}</p>
     <div>
-<!--      v-model="msgで、this.msgに保管-->
+      <!--      v-model="msgで、this.msgに保管-->
       <input type="text" v-model="msg" />
-<!-- @clickでdoClickを呼び出す-->
+      <!-- @clickでdoClickを呼び出す-->
       <button @click="doClick">Click</button>
     </div>
-    <hr>
+    <hr />
     <table>
       <tr>
         <th>User ID</th>
@@ -31,79 +31,85 @@
 </template>
 
 <script>
-    const axios = require('axios');
-    // 一覧のURL
-    let url = "https://jsonplaceholder.typicode.com/posts/";
+const axios = require("axios");
+// 一覧のURL
+let url = "https://jsonplaceholder.typicode.com/posts/";
 
-    export default {
-        data: function () {
-            return {
-                title: 'Axios',
-                message: 'axios sample',
-                msg: '',
-                json_data: {}
-            }
-        },
-        // async asyncData() {
-        //     let id = 10; // id番号
-        //     // await axios.getで、サイトの"https://jsonplaceholder.typicode.com/posts/10"とかにアクセスしてresultに代入。
-        //     let result = await axios.get(url + id);
-        //     // result.dataに、オブジェクトとしてマージされている。json_dataに入れてそのまま使える。
-        //     return {json_data: result.data};
-        // }
+export default {
+  data: function() {
+    return {
+      title: "Axios",
+      message: "axios sample",
+      msg: "",
+      json_data: {}
+    };
+  },
+  // async asyncData() {
+  //     let id = 10; // id番号
+  //     // await axios.getで、サイトの"https://jsonplaceholder.typicode.com/posts/10"とかにアクセスしてresultに代入。
+  //     let result = await axios.get(url + id);
+  //     // result.dataに、オブジェクトとしてマージされている。json_dataに入れてそのまま使える。
+  //     return {json_data: result.data};
+  // }
 
-        methods: {
-            doClick: function (event) {
-                // v-modelで保管されたmsgをthis.msgで取り出して、変数urlにくっつけてアクセスしている。
-                axios.get(url + this.msg).then((res) => {
-                    this.message = 'get ID=' + this.msg;
-                    // then内のアロー関数で、引数のresからdataを取り出し、this.json_dataに設定している
-                    this.json_data = res.data;
-                })
-            }
-        }
+  methods: {
+    doClick: function(event) {
+      // v-modelで保管されたmsgをthis.msgで取り出して、変数urlにくっつけてアクセスしている。
+      axios
+        .get(url + this.msg)
+        .then(res => {
+          this.message = "get ID=" + this.msg;
+          // then内のアロー関数で、引数のresからdataを取り出し、this.json_dataに設定している
+          this.json_data = res.data;
+        })
+        .catch(error => {
+          this.message = "Error!";
+          this.json_data = {};
+        });
     }
+  }
+};
 </script>
 
 <style>
-  .container {
-    padding: 5px 10px;
-  }
-  h1 {
-    font-size: 60pt;
-    color: #345980;
-  }
-  p {
-    padding-top:5px;
-    font-size: 20pt;
-  }
-  div {
-    font-size:14pt;
-  }
-  pre {
-    padding: 10px;
-    font-size: 18pt;
-    background-color: #efefef;
-    white-space: pre-wrap ;
-  }
-  hr {
-    margin:10px 0px;
-  }
-  tr th {
-    width:150px;
-    background-color: darkblue;
-    color:white;
-    font-size:16pt;
-  }
-  tr td {
-    padding:5px 10px;
-    background-color:#eef;
-    font-size:14pt;
-  }
-  input {
-    font-size:14pt;
-  }
-  button {
-    font-size:14pt;
-  }
+.container {
+  padding: 5px 10px;
+}
+h1 {
+  font-size: 60pt;
+  color: #345980;
+}
+p {
+  padding-top: 5px;
+  font-size: 20pt;
+}
+div {
+  font-size: 14pt;
+}
+pre {
+  padding: 10px;
+  font-size: 18pt;
+  background-color: #efefef;
+  white-space: pre-wrap;
+}
+hr {
+  margin: 10px 0px;
+}
+tr th {
+  width: 150px;
+  background-color: darkblue;
+  color: white;
+  font-size: 16pt;
+}
+tr td {
+  padding: 5px 10px;
+  background-color: #eef;
+  font-size: 14pt;
+}
+input {
+  font-size: 14pt;
+}
+button {
+  font-size: 14pt;
+}
 </style>
